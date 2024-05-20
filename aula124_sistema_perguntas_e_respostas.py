a8cc1cea41f8bb24b1f137994eb4f5ffa1e68066
Pedro Victor <pedrovictor.cortez@gmail.com>
2024-05-20 11:47:49 -0300

    Exercício de fixação

--- aula124_sistema_perguntas_e_respostas.py
+++ aula124_sistema_perguntas_e_respostas.py
@@ -1,7 +1,38 @@
-questoes = {
-    [
-        'enunciado': 'Quem descobriu o Brasi?'
-        'opcoes': ['Cabral', 'Colombo', 'Pedro I', 'Pedro II'],
-        'resposta': 'Cabral'
-    ]
-}
+teste = [ 
+    {
+        'Enunciado': 'Quem descobriu o Brasi?', 'Opcões': ['Cabral', 'Colombo', 'Pedro I', 'Pedro II'], 'resposta': '0'
+    },
+    {
+        'Enunciado': 'Qual destes estados não pertence à região NE?',
+        'Opcões': ['PI', 'MA', 'CE', 'SP'],
+        'resposta': '3',
+    },
+    {
+        'Enunciado': 'Qual a língua oficial do Brasil?',
+        'Opcões': ['Inglês', 'Português', 'Francês', 'Espanhol'],
+        'resposta': '1',
+    }
+]                
+            
+acerto = 0
+erro = 0
+
+for questao in teste:
+    print('\n', questao['Enunciado'], '\n')
+    
+    for indice, alternativa in enumerate(questao['Opcões']):
+        print(f'{indice}) {alternativa}')
+
+    chute = input('\nEscolha apenas uma das alternativas: ')
+
+    if chute == questao['resposta']:
+        print('\nResposta CERTA!')
+        acerto += 1
+    else:
+        print('\nResposta ERRADA!')
+        erro += 1
+
+print(
+    f'\nVocê acertou {acerto} questão(ões)\n'
+    f'Você errou {erro} questão(ões)'
+    )
